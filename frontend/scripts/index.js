cb1088314cf4237891764126c466c9aa1287eaad
Baptiste Biancardino <baptiste.biancardino@gmail.com>
2021-08-19 21:11:48 +0200

    Ajout police

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -5,7 +5,7 @@ function main() {
 }
 
 // Récupérer les articles depuis l'API
-function getArticles() {
+/*function getArticles() {
   fetch("http://localhost:3000/api/teddies")
     .then(function (res) {
       return res.json();
@@ -16,7 +16,7 @@ function getArticles() {
         "Nous n'avons pas réussi à afficher nos nounours. Avez vous bien lancé le serveur local (Port 3000) ? <br>Si le problème persiste, contactez-nous.";
       productsContainer.style.textAlign = "center";
       productsContainer.style.padding = "30vh 0";
-    })
+    })*/
 
     // Dispatcher les données de chaque produit (prix, nom...) dans le DOM
     .then(function (resultatAPI) {
